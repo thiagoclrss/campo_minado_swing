4b61a0a03aca592bb7e083376a1f69138d7e4713
thiagoclrss <thiagocolaress77@gmail.com>
2023-08-02 11:48:02 -0300

    update

---
+++ src/br/com/meucurso/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,14 @@
+package br.com.meucurso.cm.visao;
+
+import javax.swing.JPanel;
+
+import br.com.meucurso.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel{
+
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+		
+	}
+	
+}
